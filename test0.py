20dda51467257ce15c8e692a40cd5dea3f6c3d2b
kamccall <kmccall@microsoft.com>
2017-12-15 06:59:30 -0800

    very important comments here... km

--- test0.py
+++ test0.py
@@ -2,8 +2,11 @@
 
 name = raw_input('enter your name: ')
 print("hello " + name + "!")
-a = 10
-b = 20
+a = 32
+print("a is: " + str(a))
+b = 32
+print("b is: " + str(b))
 c = a + b
-print("the number should be 30: " + str(c) + "\n")
+print("c is: " + str(c))
+# print("the number should be 64: " + str(c) + "\n")
 print("goodbye...")
